fb138f2915727ee5c5c2827a2f7455f4a17285cf
hyukjin0419 <21900771@handong.ac.kr>
2025-02-04 22:14:36 +0900

    필요없는 import 제거

--- src/main/java/allowance_manager/allowance_manager/domain/Budget.java
+++ src/main/java/allowance_manager/allowance_manager/domain/Budget.java
@@ -3,7 +3,6 @@ package allowance_manager.allowance_manager.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 
