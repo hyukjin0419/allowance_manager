8496f3c296f2f8466a46e1684e36f9482b406808
hyukjin0419 <21900771@handong.ac.kr>
2025-03-02 20:19:36 +0900

    empty commit

--- src/main/java/allowance_manager/allowance_manager/repository/MemberRepository.java
+++ src/main/java/allowance_manager/allowance_manager/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import allowance_manager.allowance_manager.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 import java.util.Optional;
 
 @Repository
